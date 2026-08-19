a65a116d6585183dfdf2820c0446f6a9053524fa
Jacob-Staff <160220008+Jacob-Staff@users.noreply.github.com>
2026-08-19 23:17:42 +0200

    Update out_of_stock.php

--- dashboard/out_of_stock.php
+++ dashboard/out_of_stock.php
@@ -203,10 +203,10 @@ require_once "../includes/head.php";
             </div>
 
         </div
-            <?php 
+    </div>
+                <?php 
 if (file_exists("../includes/footer.php")) require_once "../includes/footer.php"; 
 ?>
-    </div>
 </div>
 <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
