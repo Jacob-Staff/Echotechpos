9780e64835c1ecd37bcd4740784eab629ece70dd
Jacob-Staff <160220008+Jacob-Staff@users.noreply.github.com>
2026-08-19 18:17:30 +0200

    Refactor footer inclusion in sell_now.php

--- dashboard/sell_now.php
+++ dashboard/sell_now.php
@@ -304,12 +304,11 @@ require_once "../includes/head.php";
                 <button type="button" class="btn btn-success btn-sm flex-grow-1" onclick="window.print()">Print</button>
             </div>
         </div>
-    </div>
-</div>
-
-<?php 
+        <?php 
 if (file_exists("../includes/footer.php")) require_once "../includes/footer.php"; 
 ?>
+    </div>
+</div>
 
 <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
