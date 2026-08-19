8ec9060dde0e2c8eaafa1d3b8966ed8dec4bdd6c
Jacob-Staff <160220008+Jacob-Staff@users.noreply.github.com>
2026-08-19 23:21:11 +0200

    Fix HTML structure in out_of_stock.php

--- dashboard/out_of_stock.php
+++ dashboard/out_of_stock.php
@@ -201,12 +201,11 @@ require_once "../includes/head.php";
                     </table>
                 </div>
             </div>
-
-        </div
-    </div>
                 <?php 
 if (file_exists("../includes/footer.php")) require_once "../includes/footer.php"; 
 ?>
+        </div
+    </div>
 </div>
 <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
