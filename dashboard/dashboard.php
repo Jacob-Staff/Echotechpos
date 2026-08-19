d81f1531dee8580d119eff946f580b68c897c790
Jacob-Staff <160220008+Jacob-Staff@users.noreply.github.com>
2026-08-19 14:00:51 +0200

    Add footer component inclusion in dashboard.php

--- dashboard/dashboard.php
+++ dashboard/dashboard.php
@@ -139,13 +139,13 @@ require_once "../includes/head.php";
                 </div>
             </div>
         </div>
-    </div>
-    <?php 
+            <?php 
                     // Add your footer component here
     if (file_exists("../includes/footer.php")) {
         require_once "../includes/footer.php"; 
     }
     ?>
+    </div>
 </div>
 
 <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
