f8a55f647c3c1b7e4af8926141e59bdfac06661e
Jacob-Staff <160220008+Jacob-Staff@users.noreply.github.com>
2026-08-19 16:33:19 +0200

    Update dashboard.php

--- dashboard/dashboard.php
+++ dashboard/dashboard.php
@@ -208,13 +208,12 @@ require_once "../includes/head.php";
             </div>
         </form>
     </div>
-</div>
-
-<?php 
+    <?php 
 if (file_exists("../includes/footer.php")) {
     require_once "../includes/footer.php"; 
 }
 ?>
+</div>
 
 <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
