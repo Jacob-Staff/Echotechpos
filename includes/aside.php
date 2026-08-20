3e66353df9689c890cd2df668ee8d4c34a23d2e3
Jacob-Staff <160220008+Jacob-Staff@users.noreply.github.com>
2026-08-21 00:58:41 +0200

    Update aside.php

--- includes/aside.php
+++ includes/aside.php
@@ -56,7 +56,7 @@ if (isset($conn) && $conn) {
                     </a>
                 </li>
                 <li class="sidebar-item mb-1">
-                    <a class="sidebar-link" href="purchase_orders.php">
+                    <a class="sidebar-link" href="purchase_orders_list.php">
                         <i class="mdi mdi-cart-outline"></i><span>Purchase-orders</span>
                     </a>
                 </li>
