b3b34914b88e2d841a9ea43bdb624ebfad213408
Jacob-Staff <160220008+Jacob-Staff@users.noreply.github.com>
2026-08-21 13:16:54 +0200

    Fix footer inclusion and clean up HTML structure

--- dashboard/dashboard.php
+++ dashboard/dashboard.php
@@ -207,13 +207,14 @@ require_once "../includes/head.php";
                 <input type="number" name="tax_rate" class="form-control form-control-sm bg-light" value="16">
             </div>
         </form>
-    </div>
-</div>
-    <?php 
+
+        <?php 
 if (file_exists("../includes/footer.php")) {
     require_once "../includes/footer.php"; 
 }
 ?>
+    </div>
+</div>
 
 <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
